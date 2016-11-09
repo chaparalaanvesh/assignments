63ed3899ba63a5a72e241ad862938a4f335f6a80
chaparalaanvesh <chaparalaanvesh@gmail.com>
2016-11-09 10:15:39 -0500

    surprisetest

--- sample1.py
+++ sample1.py
@@ -1,5 +1,11 @@
-inputstring = raw_input("enter string: ")
+input_string = raw_input("enter input string: ")
+output = []
 count = 1
-length=[]
-for i in range(1,len(inputstring)):
-    print i
+for i in range(1, len(input_string)):
+    if input_string[i] == input_string[i - 1]:
+        count = count + 1
+    else:
+        output.append('(' + input_string[i - 1] + "-" + str(count) + ')')
+        count = 1
+output.append('(' + input_string[i] + "-" + str(count) + ')')
+print (output)
--- surprisetest.py
+++ surprisetest.py
@@ -1,11 +1,13 @@
 input_string = raw_input("enter input string: ")
+output = []
 count = 1
-output=[]
-for i in range(1,len(input_string)):
-    if input_string[i-1] == input_string[i]:
-       count = count + 1
-    else :
-        output.append(input_string[i-1]+"-"+str(count))
+for i in range(1, len(input_string)):
+    print i
+    if input_string[i - 1] == input_string[i]:
+        count = count + 1
+    else:
+        output.append(  input_string[i - 1] + "-" + str(count) )
         count = 1
-output.append(input_string[i]+"-"+str(count))
-print (output)
+output.append(input_string[i] + "-" + str(count))
+print output
+
