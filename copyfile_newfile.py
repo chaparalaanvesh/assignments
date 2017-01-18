23e55f0883289fe505ffe8664ba045c2ed76bd47
chaparalaanvesh <chaparalaanvesh@gmail.com>
2017-01-18 13:12:09 -0500

    copying of files from one to the other new file

---
+++ copyfile_newfile.py
@@ -0,0 +1,12 @@
+"""Write a program that given a text file will create a new text file in which all the lines from the original file are
+numbered from 1 to n (where n is the number of lines in the file)."""
+
+def copyfile_newfile(f):
+    old_file = open('C:\Python27\len_of_words.txt')
+    new_file = open('C:\Python27\ newfile.txt', 'wt')
+    for line, text in enumerate(old_file):
+        new_file.write('%s %s' % (line + 1,text))
+    old_file.close()
+    new_file.close()
+
+copyfile_newfile(f='C:\Python27\ newfile.txt')
