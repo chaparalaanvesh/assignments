7bb774033f8de36e550b1f173cf0e36cf8c27dbd
chaparalaanvesh <chaparalaanvesh@gmail.com>
2017-02-07 12:23:00 -0500

    to draw a circle using turtle method

---
+++ draw_circle.py
@@ -0,0 +1,14 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor('red')
+
+    brad = turtle.Turtle
+    brad.shape('turtle')
+    brad.color('yellow')
+    brad.circle(200)
+
+    window.exitonclick()
+
+draw_square()
