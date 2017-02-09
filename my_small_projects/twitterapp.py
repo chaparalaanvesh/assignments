243fd597cc9da33d2bbd051b898a3b2639ecd483
chaparalaanvesh <chaparalaanvesh@gmail.com>
2017-02-09 10:10:44 -0500

    updated code using try and catch blocks

--- my_small_projects/twitterapp.py
+++ my_small_projects/twitterapp.py
@@ -1,40 +1,70 @@
-from twython import Twython
-
+from twython import Twython,TwythonError
+import configparser
+                                                                                        #config lib , logging to log all the operations , try except blocks
 
 class twitterthon:
     def __init__(self):
 
-        APP_KEY = '37Dc2q5kcUBhaAKtHaRo6zgtF'
-        APP_SECRET = 'PWL1zNmJLSUaL2qGzCW2UzGSMAcReQ2v5WCY40swaamk2oHklb'
-        twitter = Twython(APP_KEY, APP_SECRET,oauth_version=2)
+        config = configparser.ConfigParser()
+        config.read('keys.txt')
+        APP_KEY = config.get('key','APP_KEY')
+        APP_SECRET = config.get('key','APP_SECRET')
+        twitter = Twython(APP_KEY,APP_SECRET,oauth_version=2)
         ACCESS_TOKEN = twitter.obtain_access_token()
-        self.twitter = Twython(APP_KEY,access_token= ACCESS_TOKEN)
+        self.twitter = Twython(APP_KEY,APP_SECRET,access_token = ACCESS_TOKEN)
 
-    def followers(self):
-        user=raw_input("Please enter a twitter name:")
+    def followers(self,screen_name):
+       followers = []
 
-        output = self.twitter.get_followers_list(screen_name=user)
-        for each in output['users']:
-            print each['screen_name']
+       try:
+           followers_data = self.twitter.get_followers_list(screen_name=screen_name)
+           followers = [each['screen_name'] for each in followers_data['users']]
 
-    def searching_tweet(self):
-        user = raw_input("Please enter a string to search tweets :")
-        output = self.twitter.search(q=user)
-        print output
-        for tweet in output['statuses']:
-            print tweet['text']
+        except TwythonError as error:
+           print error
+
+        return followers
+
+    def tweets_list(self,screen_name):
+        tweets=[]
+        try:
+            result = self.twitter.get_user_timeline(screen_name=screen_name)
+            tweets = [i['text'] for i in result]
+
+        except TwythonError as error:
+            print error
+
+        return tweets
+
+    def search_tweets(self, screen_name, search_text):
+        search_tweet_result = []
+        try:
+            result = self.twitter.get_user_timeline(screen_name=screen_name)
+            search_tweet_result = [i['text'] for i in result if search_text in i['text']]
+
+        except TwythonError as error:
+            print error
+
+        return search_tweet_result
 
     def gettingsimilarUser(self):
-        user1 = raw_input("Please enter a valid twitter screen name1:")
-        output1 = self.twitter.get_followers_ids(screen_name=user1)
-        user2 = raw_input("Please enter another valid twitter screen name2:")
-        output2 = self.twitter.get_followers_ids(screen_name=user2)
-        for i in output1["ids"]:
-            for j in output2["ids"]:
-                data1 = self.twitter.show_user(user=i)
-                data2 = self.twitter.show_user(user=j)
+        similar_users = []
+        try:
+            user1 = self.twitter.get_followers_ids(screen_name=user_1)
+            user2 = self.twitter.get_followers_ids(screen_name=user_2)
+            user1_followers = [i['screen_name'] for i in user1['users']]
+            user2_followers = [i['screen_name'] for i in user2['users']]
+            for i in user1_followers:
+                for j in user2_followers:
+                    data1 = self.twitter.show_user(user=i)
+                    data2 = self.twitter.show_user(user=j)
                 if data1 ==data2:
                     print(data1["screen_name"])
+
+        except TwythonError as error:
+            print error
+
+        return similar_users
 
 
 t = twitterthon()
