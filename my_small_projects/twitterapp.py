421eb8e81693c2cc505a540421c947e91a4486cc
chaparalaanvesh <chaparalaanvesh@gmail.com>
2017-02-12 17:11:37 -0500

    twitters api

--- my_small_projects/twitterapp.py
+++ my_small_projects/twitterapp.py
@@ -1,14 +1,17 @@
-from twython import Twython,TwythonError
+from twython import Twython
 import configparser
+import logging
+
                                                                                         #config lib , logging to log all the operations , try except blocks
+
 
 class twitterthon:
     def __init__(self):
 
         config = configparser.ConfigParser()
         config.read('keys.txt')
-        APP_KEY = config.get('key','APP_KEY')
-        APP_SECRET = config.get('key','APP_SECRET')
+        APP_KEY = config.get('app_key','APP_KEY')
+        APP_SECRET = config.get('app_key','APP_SECRET')
         twitter = Twython(APP_KEY,APP_SECRET,oauth_version=2)
         ACCESS_TOKEN = twitter.obtain_access_token()
         self.twitter = Twython(APP_KEY,APP_SECRET,access_token = ACCESS_TOKEN)
@@ -20,19 +23,19 @@ class twitterthon:
            followers_data = self.twitter.get_followers_list(screen_name=screen_name)
            followers = [each['screen_name'] for each in followers_data['users']]
 
-        except TwythonError as error:
-           print error
+       except Exception:
+           logging.error('error in the followers function')
 
-        return followers
+       return followers
 
-    def tweets_list(self,screen_name):
+    def tweets_list(self,screen_name):          #doc strings
         tweets=[]
         try:
-            result = self.twitter.get_user_timeline(screen_name=screen_name)
+            result = self.twitter.get_user_timeline(screen_name=screen_name)   #limit
             tweets = [i['text'] for i in result]
 
-        except TwythonError as error:
-            print error
+        except Exception, e:
+            logging.error('there is an error in the tweets_list function',str(e))
 
         return tweets
 
@@ -42,27 +45,27 @@ class twitterthon:
             result = self.twitter.get_user_timeline(screen_name=screen_name)
             search_tweet_result = [i['text'] for i in result if search_text in i['text']]
 
-        except TwythonError as error:
-            print error
+        except Exception, e:
+            logging.error('error present in the search_tweets function',str(e))
 
         return search_tweet_result
 
-    def gettingsimilarUser(self):
+    def gettingsimilarUser(self,user1,user2):
         similar_users = []
         try:
-            user1 = self.twitter.get_followers_ids(screen_name=user_1)
-            user2 = self.twitter.get_followers_ids(screen_name=user_2)
-            user1_followers = [i['screen_name'] for i in user1['users']]
-            user2_followers = [i['screen_name'] for i in user2['users']]
+            user_1 = self.twitter.get_followers_ids(screen_name=user1)
+            user_2 = self.twitter.get_followers_ids(screen_name=user2)
+            user1_followers = [i['screen_name'] for i in user_1['users']]
+            user2_followers = [i['screen_name'] for i in user_2['users']]
             for i in user1_followers:
                 for j in user2_followers:
                     data1 = self.twitter.show_user(user=i)
                     data2 = self.twitter.show_user(user=j)
-                if data1 ==data2:
+                if data1 in data2:
                     print(data1["screen_name"])
 
-        except TwythonError as error:
-            print error
+        except Exception, e:
+            logging.error('erroe in getting similar users',str(e))
 
         return similar_users
 
