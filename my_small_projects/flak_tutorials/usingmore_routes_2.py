647db555872e6d78817dfbc40c6cf5c6e0dbcb33
chaparalaanvesh <chaparalaanvesh@gmail.com>
2017-02-12 18:27:18 -0500

    .

---
+++ my_small_projects/flak_tutorials/usingmore_routes_2.py
@@ -0,0 +1,23 @@
+from flask import Flask:
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'this is home page'
+
+@app.route('/anvi')
+def anvi():
+    return 'This is anvesh learning the Flask'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return 'hey there %s' % username
+
+@app.route('/profile_id/<int:id')
+def profile_id(id):
+    return 'hey there %d' % id
+
+
+if __name__ == '__main__'
+    app.run(debug=True)
